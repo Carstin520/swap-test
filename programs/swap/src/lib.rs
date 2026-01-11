fbd4688ada95a86516de1fc87bd29162b55347d7
Carstin520 <jamescarstin520@gmail.com>
2026-01-11 22:00:06 +0800

    completed instructions and transfer logics

--- programs/swap/src/lib.rs
+++ programs/swap/src/lib.rs
@@ -13,9 +13,12 @@ declare_id!("4F6SYakdp8jYbvSTMaWsFgBp5Vknp6hAjCgp4GsQzhPx");
 
 #[program]
 pub mod swap {
+    use crate::instruction::MakeOffer;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(ctx: Context<MakeOffer>) -> Result<()> {
+        instruction::make_offer::send_offer_tokens_to_vault()?;
+        instruction::make_offer::save_offer()?;
     }
 }
